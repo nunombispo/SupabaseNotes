438efe9d8b9a5f160a5f97d9faafc13b6deddecf
Nuno Bispo <nuno.m.bispo@gmail.com>
2021-10-05 23:43:15 +0200

    Added Bootstrap and Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
+import Header from "./Header";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Header/>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+function Header() {
+    const divStyle={backgroundColor: 'teal'}
+    return (
+        <nav className="navbar navbar-dark" style={divStyle}>
+            <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">Navbar</span>
+            </div>
+        </nav>
+    );
+  }
+  
+  export default Header;
