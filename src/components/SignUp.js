039884ca07fb61c6061c910fed46544cc52253d0
Nuno Bispo <nuno.m.bispo@gmail.com>
2021-10-08 20:59:57 +0200

    Signup Working

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,31 @@
 import { useRef, useState } from 'react'
+import { useHistory, Link } from 'react-router-dom'
+import { useAuth } from '../contexts/Auth'
 
 export function Signup() {
   const emailRef = useRef()
   const passwordRef = useRef()
 
+  // Get signUp function from the auth context
+  const { signUp } = useAuth()
+  const history = useHistory()
+
   async function handleSubmit(e) {
     e.preventDefault()
 
-    // @TODO: add sign up logic
+    // Get email and password input values
+    const email = emailRef.current.value
+    const password = passwordRef.current.value
+
+    // Calls `signUp` function from the context
+    const { error } = await signUp({ email, password })
+
+    if (error) {
+      alert('error signing up')
+    } else {
+      // Redirect user to Dashboard
+      history.push('/')
+    }
   }
 
   return (
@@ -16,18 +34,18 @@ export function Signup() {
             <div className="col d-none d-lg-block"></div>
             <div className="col">
                 <form onSubmit={handleSubmit}>
-                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                    <h1 className="h3 mb-3 fw-normal">Please sign up</h1>
 
                     <div className="form-floating mb-3">
-                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                        <label for="floatingInput">Email address</label>
+                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" ref={emailRef}/>
+                        <label htmlFor="floatingInput">Email address</label>
                     </div>
                     <div className="form-floating mb-3">
-                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                        <label for="floatingPassword">Password</label>
+                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" ref={passwordRef}/>
+                        <label htmlFor="floatingPassword">Password</label>
                     </div>
 
-                    <button className="w-100 btn btn-lg btn-primary mb-3" type="submit">Sign in</button>
+                    <button className="w-100 btn btn-lg btn-success mb-3" type="submit">Sign up</button>
                 </form>
             </div>
             <div className="col d-none d-lg-block"></div>
