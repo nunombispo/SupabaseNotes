698e1fc51126bb8be6203e1b81e359a7cb80744e
Nuno Bispo <nuno.m.bispo@gmail.com>
2021-10-09 13:29:32 +0200

    SignIn working

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { AuthProvider } from '../contexts/Auth'
 import Header from './Header'
 import Notes from './Notes'
 import { Signup } from './SignUp'
+import { Signin } from './SignIn'
 
 export function App() {
   return (
@@ -13,6 +14,7 @@ export function App() {
           <Switch>
             <Route exact path="/" component={Notes} />
             <Route path="/signup" component={Signup} />
+            <Route path="/signin" component={Signin} />
           </Switch>
         </AuthProvider>
       </Router>
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,12 +2,12 @@ import { useRef, useState } from 'react'
 import { useHistory, Link } from 'react-router-dom'
 import { useAuth } from '../contexts/Auth'
 
-export function Signup() {
+export function Signin() {
   const emailRef = useRef()
   const passwordRef = useRef()
 
-  // Get signUp function from the auth context
-  const { signUp } = useAuth()
+  // Get signIn function from the auth context
+  const { signIn } = useAuth()
   const history = useHistory()
 
   async function handleSubmit(e) {
@@ -17,11 +17,11 @@ export function Signup() {
     const email = emailRef.current.value
     const password = passwordRef.current.value
 
-    // Calls `signUp` function from the context
-    const { error } = await signUp({ email, password })
+    // Calls `signIn` function from the context
+    const { error } = await signIn({ email, password })
 
     if (error) {
-      alert('error signing up')
+      alert('error signing in')
     } else {
       // Redirect user to Dashboard
       history.push('/')
@@ -34,7 +34,7 @@ export function Signup() {
             <div className="col d-none d-lg-block"></div>
             <div className="col">
                 <form onSubmit={handleSubmit}>
-                    <h1 className="h3 mb-3 fw-normal">Please sign up</h1>
+                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
                     <div className="form-floating mb-3">
                         <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" ref={emailRef}/>
                         <label htmlFor="floatingInput">Email address</label>
@@ -43,7 +43,7 @@ export function Signup() {
                         <input type="password" className="form-control" id="floatingPassword" placeholder="Password" ref={passwordRef}/>
                         <label htmlFor="floatingPassword">Password</label>
                     </div>
-                    <button className="w-100 btn btn-lg btn-success mb-3" type="submit">Sign up</button>
+                    <button className="w-100 btn btn-lg btn-success mb-3" type="submit">Sign in</button>
                 </form>
             </div>
             <div className="col d-none d-lg-block"></div>
